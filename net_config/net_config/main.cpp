e6fbe19ae5c97ac57d752760d2908d16006e89f9
w123l123h123 <joe0636@sina.com>
2019-08-09 16:07:50 +0800

    0809

---
+++ net_config/net_config/main.cpp
@@ -0,0 +1,44 @@
+
+#include <iostream>
+#include <locale>
+#include <string>
+#include <codecvt>
+#include "net_config.h"
+
+#pragma execution_character_set("utf-8")
+
+using namespace std;
+
+int main()
+{
+	std::locale::global(std::locale(""));
+
+	NetConfig config;
+	config.set_key("{cd574b17-73e1-42df-a50c-f95d7f15c1d0}");
+
+	if (config.enable_dhcp()) {
+		cout << "enable_dhcp success" << endl;
+
+	}
+	else {
+		cout << "error code: " << config.get_last_error_code() << endl;
+	}
+
+	
+	if (config.set_ip_config("192.168.1.100", "255.255.255.0", "192.168.1.1")) {
+		cout << "set_ip_config success" << endl;
+	}
+	else {
+		cout << "error code: " << config.get_last_error_code() << endl;
+	}
+
+	if (config.set_dns("255.255.255.0", "")) {
+		cout << "set_dns success" << endl;
+	}
+	else {
+		cout << "error code: " << config.get_last_error_code() << endl;
+	}				
+	
+	cin.get();
+	return 0;
+}
